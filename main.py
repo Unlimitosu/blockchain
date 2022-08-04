415cfd62e8378148112597a189515d0e5ce39d41
Unlimit <93829175+Unlimitosu@users.noreply.github.com>
2022-08-04 18:20:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import chain
 
 def main():
-    block = chain.BlockChain
+    block = chain.BlockChain()
     block.new_block()
     block.new_block()
-    block.print_chain
+    block.print_chain()
 
 if __name__=="__main__":
     main()
