100f2e34e81e36a3758ad324d9d3cf9a91469e86
Unlimit <93829175+Unlimitosu@users.noreply.github.com>
2022-08-16 10:19:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import platform
 def main():
     block = chain.BlockChain(5)
     block.print_chain()
-
+    if block.vaild_transaction():
+        print("ok")
+    
 if __name__=="__main__":
     main()
+# EOF
